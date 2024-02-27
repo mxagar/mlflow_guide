fa4688c88023aa1f697fde5bdee547f6a04bcec2
Mikel Sagardia <sagardia.mikel@gmail.com>
2024-02-27 14:42:08 +0100

    feat: basic tracking example (reg LR) with wine dataset

---
+++ examples/01_tracking/basic_regression.py
@@ -0,0 +1,67 @@
+'''Basic Linear Regression, regularized with ElasticNet.
+
+Code from the Udemy course
+
+    MLflow in Action - Master the art of MLOps using MLflow tool
+    https://www.udemy.com/course/mlflow-course/
+
+Author: J Garg, Real Time Learning
+'''
+
+import warnings
+import argparse
+import logging
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet # Regularized Linear Regression, combined L1 & L2
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+# Get arguments from command
+parser = argparse.ArgumentParser()
+parser.add_argument("--alpha", type=float, required=False, default=0.5) # learning rate
+parser.add_argument("--l1_ratio", type=float, required=False, default=0.5) # ratio L1, L2
+args = parser.parse_args()
+
+# Evaluation function
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    # Read the wine-quality csv file from local
+    data = pd.read_csv("red-wine-quality.csv")
+    data.to_csv("red-wine-quality.csv", index=False)
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    alpha = args.alpha
+    l1_ratio = args.l1_ratio
+
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
